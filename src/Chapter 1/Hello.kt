a77d13360d498c9e0b5f8b3de89b9550ba7be83d
septemblue <focusramxram@gmail.com>
2023-02-14 19:43:22 +0700

    chapter 1

--- src/Chapter 1/Hello.kt
+++ src/Chapter 1/Hello.kt
@@ -1,3 +1,23 @@
 fun main() {
     println("Hello, World")
+
+    // contoh while
+    var x = 3
+    while(x > 0) {
+        println("nilai x: $x")
+        x -= 1
+    }
+
+    // contoh for
+    for (x in 1..3) {
+        println("nilai x: $x")
+    }
+
+    // contoh if
+    x = 4;
+    val y = 5;
+    if (x < y) println("Yes $x is bigger than $y") else println("No $x is not bigger than $y")
+    
+
+
 }
